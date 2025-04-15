e254f70376c86f2fdbd4b2e8da51302b72550200
Doğu Abaris <abaris@null.net>
2025-04-15 16:42:08 +0200

    next.config.ts: Update the base path for website

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig: NextConfig = {
     images: {
         unoptimized: true
     },
-    basePath: '/corev-website',
-    assetPrefix: '/corev-website/',
 };
 
 export default nextConfig;
